7c972e079b36b7e283047f827bdf33292f3262d3
zhangyingtao75144 <125929019@qq.com>
2020-11-24 10:04:50 +0800

    修改广告期号

--- application/small/model/PubAdsBox.php
+++ application/small/model/PubAdsBox.php
@@ -31,7 +31,7 @@ class PubAdsBox extends Base
         ->field("b.create_time")
         ->where('a.box_id='.$box_id." and b.end_date>'".$now_date.
                 "' and b.state=1 ")
-        ->order('b.start_date desc')
+        ->order('b.id desc')
         ->limit(1)
         ->find();
         return $data;
